188208c7c488e36f87f04b1024df1116782e425f
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-16 20:12:22 +0530

    Done running loop in Angular

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
+  todos: Todo[];
 
+  constructor() {
+    this.todos = [
+      {
+        sno: 3,
+        title: "This is a title",
+        desc: "Description",
+        active: true
+      },
+         {
+        sno: 4,
+        title: "This is a title 2",
+        desc: "Description",
+        active: true
+      },   {
+        sno: 5,
+        title: "This is a title 3",
+        desc: "Description",
+        active: true
+      },
+    ]
+  }
+  ngOnInit():void{}
 }
